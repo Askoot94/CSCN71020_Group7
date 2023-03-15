ac424729957e8ead1bd1644be809088e98b89e32
Yinus Alamutu <Yalamutu2169@conestogac.on.ca>
2023-03-15 10:41:15 -0400

    Formula Change

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -22,3 +22,22 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 
 	return result;
 }
+int* anglesinTriangle(int side1, int side2, int side3) {
+	int* result = "";
+	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+		result = 0;
+	}
+	else if (side1 == side2 && side1 == side3) {
+		result = "Each angle is 60 degrees";
+	}
+	else if ((side1 == side2 && side1 != side3) ||
+		(side1 == side3 && side1 != side2))
+	{
+		result = "2 angles are equal and the 3rd angle is different";
+	}
+	else
+	{
+		result = " The 3 angles are different";
+	}
+	return result;
+}

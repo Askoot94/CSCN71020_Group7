1bcb796f4f32fd0517c90e6dc0c3c09415889dd8
Askoot94 <98133009+Askoot94@users.noreply.github.com>
2023-03-16 12:54:08 -0400

    Adjusted analyzeRectangle

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -4,9 +4,13 @@
 #include <stdlib.h>
 
 char* analyzeRectangle(int *xValue, int *yValue) {
+	//Store Lengths of each side in their own variables
 	int topLength = abs(xValue[0] - xValue[1]);
-	int botLength = abs(xValue[2], xValue[3]);
+	int botLength = abs(xValue[2] - xValue[3]);
 
-	int leftLength = abs(xValue[0] - xValue[1]);
-	int rightLength = abs(xValue[2], xValue[3]);
+	int leftLength = abs(yValue[0] - yValue[3]);
+	int rightLength = abs(yValue[1] - yValue[2]);
+
+	//Check that topLength and botLength as well as leftLength and rightLength are equal
+
 }

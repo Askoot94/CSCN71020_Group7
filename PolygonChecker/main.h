3781a9c7e8b84c01e5bc0c8142cac34e2dfaa3b1
joelregiabraham <jregiabraham5574@conestogac.on.ca>
2023-03-16 11:32:40 -0400

    Keep

--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -2,3 +2,7 @@
 void printWelcome();
 int printShapeMenu();
 int* getTriangleSides(int* triangleSides);
+<<<<<<< HEAD
+=======
+int* getRectangleSides(int* rectangleSides);
+>>>>>>> b0a50cc1d2d7b2cecdf5bd5c1d85ce3b18dc17f5

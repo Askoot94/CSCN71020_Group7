39c2128eb41825dcc942453bb18d9ec69d86b569
Askoot94 <98133009+Askoot94@users.noreply.github.com>
2023-03-22 10:45:45 -0400

    Finalized analyzeRectangle function in accordance with option 1

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -13,7 +13,12 @@ char* analyzeRectangle(int *xValue, int *yValue) {
 	int rightLength = abs(yValue[1] - yValue[2]);
 
 	//Check that topLength and botLength as well as leftLength and rightLength are equal
-	if (topLength == botLength && leftLength == rightLength)
-		printf();
-
+	if (topLength == botLength && leftLength == rightLength && topLength == rightLength)
+		result = "Square";
+	else if (topLength == botLength && leftLength == rightLength)
+		result = "Rectangle";
+	else
+		result = "Not a Rectangle";
+	
+	return result;
 }

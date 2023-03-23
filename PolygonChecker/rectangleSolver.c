27737be376e00504f62473e75cc3ef545dcbf6dc
joelregiabraham <jregiabraham5574@conestogac.on.ca>
2023-03-23 11:42:20 -0400

    added perimeter function

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -14,12 +14,31 @@ char* analyzeRectangle(int *xValue, int *yValue, int rectangleSides[]) {
 
 	//Check that topLength and botLength as well as leftLength and rightLength are equal
 	if (topLength == botLength && leftLength == rightLength && topLength == rightLength) {
-		result = "Square";
+		
+		printf("\nperimeter : %d units\n", GetRectanglePerimeter(topLength, botLength, leftLength, rightLength));
+		result = "\nSquare\n\n";
 	}
 	else if (topLength == botLength && leftLength == rightLength)
-		result = "Rectangle";
-	else
+	{
+		printf("\nThe Perimeter of rectangle with Sides %d and %d ", topLength, leftLength);
+		printf("\nperimeter :  %d units\n", GetRectanglePerimeter(topLength, botLength, leftLength, rightLength));
+		result = "\nRectangle\n\n";
+	}
+	else 
 		result = "Not a Rectangle";
 	
 	return result;
+}
+
+int GetRectanglePerimeter(int topLength,int botLength, int leftLength, int rightLength)
+{
+	int perimeter = 0;
+
+	int Length_1 = topLength + botLength;
+
+	int Length_2 = leftLength + rightLength;
+
+	perimeter = Length_1 + Length_2;
+
+	return perimeter;
 }

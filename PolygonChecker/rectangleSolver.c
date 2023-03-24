99bdfc3e23b6c5a24b63f6efd0ef09863026b4d9
sulaimanolajuwo <osulaiman8207@conestogac.on.ca>
2023-03-24 01:57:41 -0400

    Wrote a function that multiplies or find the multiple of a top length and left lenght of a sqaure

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -1,9 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "rectangleSolver.h"
 #include "triangleSolver.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-char* analyzeRectangle(int *xValue, int *yValue, int rectangleSides[]) {
+char* analyzeRectangle(int* xValue, int* yValue, int rectangleSides[]) {
 	char* result = "";
 	//Store Lengths of each side in their own variables
 	int topLength = abs(xValue[0] - xValue[1]);
@@ -14,12 +15,30 @@ char* analyzeRectangle(int *xValue, int *yValue, int rectangleSides[]) {
 
 	//Check that topLength and botLength as well as leftLength and rightLength are equal
 	if (topLength == botLength && leftLength == rightLength && topLength == rightLength) {
-		result = "Square";
+
+
+		int top_length, left_length, area;
+
+		// Get user input for top and left lengths
+		printf("Enter the top length of the square: ");
+		scanf_s("%d",  &topLength);
+		printf("Enter the left length of the square: ");
+		scanf_s("%d",  &leftLength);
+
+		// Multiply top and left lengths to get the area of the square
+		area = topLength * leftLength;
+
+		result = printf("The area of the square is: %d\n", area);
+
 	}
 	else if (topLength == botLength && leftLength == rightLength)
+
 		result = "Rectangle";
-	else
+	else {
 		result = "Not a Rectangle";
-	
+	     }
+
 	return result;
+
+	return 0;
 }

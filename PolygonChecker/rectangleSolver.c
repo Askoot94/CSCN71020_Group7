54ea7918d53814bff771d661051edb400d198382
Askoot94 <98133009+Askoot94@users.noreply.github.com>
2023-04-04 12:11:37 -0400

    Merge get2 Branch
    * Wrote a function that multiplies or find the multiple of a top length and left lenght of a sqaure
    
    * temp
    
    * temp
    
    * temp
    
    * temp
    
    * temp
    
    * temp
    
    * semi final
    
    * review final code
    
    * ADjusted the get area function.
    
    * Fixed and Tested Area Calculation Funciton and display
    
    ---------
    
    Co-authored-by: sulaimanolajuwo <osulaiman8207@conestogac.on.ca>

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -1,9 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "rectangleSolver.h"
 #include "triangleSolver.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-char* analyzeRectangle(int *xValue, int *yValue, int rectangleSides[]) {
+char* analyzeRectangle(int* xValue, int* yValue, int rectangleSides[]) {
 	char* result = "";
 	//Store Lengths of each side in their own variables
 	int topLength = abs(xValue[0] - xValue[1]);
@@ -14,31 +15,39 @@ char* analyzeRectangle(int *xValue, int *yValue, int rectangleSides[]) {
 
 	//Check that topLength and botLength as well as leftLength and rightLength are equal
 	if (topLength == botLength && leftLength == rightLength && topLength == rightLength) {
-		
-		printf("\nperimeter : %d units\n", GetRectanglePerimeter(topLength, botLength, leftLength, rightLength));
+		printf("\nA square with side lengths of %d units has", topLength);
+    printf("\nArea: %d units\n", GetRectangleArea(topLength, rightLength));
+    printf("\nPerimeter : %d units\n", GetRectanglePerimeter(topLength, botLength, leftLength, rightLength));
 		result = "\nSquare\n\n";
+
 	}
-	else if (topLength == botLength && leftLength == rightLength)
-	{
-		printf("\nThe Perimeter of rectangle with Sides %d and %d ", topLength, leftLength);
-		printf("\nperimeter :  %d units\n", GetRectanglePerimeter(topLength, botLength, leftLength, rightLength));
-		result = "\nRectangle\n\n";
+	else if (topLength == botLength && leftLength == rightLength) {
+		printf("\nA rectangle with sides %d units and %d units has", topLength,rightLength);
+		printf("\nArea:  %d units\n", GetRectangleArea(topLength, rightLength));
+    printf("\nPerimeter :  %d units\n", GetRectanglePerimeter(topLength, botLength, leftLength, rightLength));
+		result  = "\nRectangle\n\n";
 	}
 	else 
 		result = "Not a Rectangle";
-	
+
 	return result;
+}
+
+
+int GetRectangleArea(int topLength,  int rightLength)
+{
+	int area = topLength * rightLength;
+	
+  return area;
 }
 
 int GetRectanglePerimeter(int topLength,int botLength, int leftLength, int rightLength)
 {
 	int perimeter = 0;
-
 	int Length_1 = topLength + botLength;
-
 	int Length_2 = leftLength + rightLength;
-
 	perimeter = Length_1 + Length_2;
 
 	return perimeter;
+
 }

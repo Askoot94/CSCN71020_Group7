c4fbd1ba41946d2cacbec13d73491e8ee750f494
joelregiabraham <jregiabraham5574@conestogac.on.ca>
2023-04-05 13:59:03 -0400

    Test Case for rectangle perimeter

--- Group7_CollectiveTestSuite/Group7_CollectiveTestSuite.cpp
+++ Group7_CollectiveTestSuite/Group7_CollectiveTestSuite.cpp
@@ -1,8 +1,9 @@
-#include "pch.h"
+                                                                                                          #include "pch.h"
 #include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 extern "C" char* analyzeTriangle(int side1, int side2, int side3);
+extern "C" int GetRectanglePerimeter(int topLength, int botLength, int leftLength, int rightLength);
 
 namespace Group7CollectiveTestSuite
 {
@@ -28,9 +29,40 @@ namespace Group7CollectiveTestSuite
 	{
 	public:
 
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Rectangle_Perimeter_1)
 		{
+			int result = 0;
+			int topLength = 2;
+			int botLength = 2;
+			int leftLength = 4;
+			int rightLength = 4;
 
+			result = GetRectanglePerimeter(topLength, botLength, leftLength,rightLength);
+			Assert::AreEqual(12, result);
+		}
+
+		TEST_METHOD(Rectangle_Perimeter_2)
+		{
+			int result = 0;
+			int topLength = 1;
+			int botLength = 1;
+			int leftLength = 8;
+			int rightLength = 3;
+
+			result = GetRectanglePerimeter(topLength, botLength, leftLength, rightLength);
+			Assert::AreEqual(13, result);
+		}
+
+		TEST_METHOD(Rectangle_Perimeter_3)
+		{
+			int result = 0;
+			int topLength = 4;
+			int botLength = 4;
+			int leftLength = 6;
+			int rightLength = 6;
+
+			result = GetRectanglePerimeter(topLength, botLength, leftLength, rightLength);
+			Assert::AreNotEqual(12, result);
 		}
 	};
 	TEST_CLASS(GiteshTestSuite)

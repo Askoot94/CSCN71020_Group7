1c8daa5d53d0ebaf81bf02e53ae4f4fbda5506a8
Askoot94 <98133009+Askoot94@users.noreply.github.com>
2023-04-05 10:53:26 -0400

    Created Individual Test Suites

--- Group7_CollectiveTestSuite/Group7_CollectiveTestSuite.cpp
+++ Group7_CollectiveTestSuite/Group7_CollectiveTestSuite.cpp
@@ -6,13 +6,49 @@ extern "C" char* analyzeTriangle(int side1, int side2, int side3);
 
 namespace Group7CollectiveTestSuite
 {
-	TEST_CLASS(Group7CollectiveTestSuite)
+	TEST_CLASS(RianTestSuite)
 	{
 	public:
 		
 		TEST_METHOD(TestMethod1)
 		{
 			
+		}
+	};
+	TEST_CLASS(YinusTestSuite)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(JoelTestSuite)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(GiteshTestSuite)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+	TEST_CLASS(JayTestSuite)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
 		}
 	};
 }

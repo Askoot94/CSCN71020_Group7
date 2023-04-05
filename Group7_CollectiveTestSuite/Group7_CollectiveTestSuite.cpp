eabbd443165f51b55c96088e47923729c4923c1c
Askoot94 <98133009+Askoot94@users.noreply.github.com>
2023-04-05 10:48:25 -0400

    Created Unit Test

---
+++ Group7_CollectiveTestSuite/Group7_CollectiveTestSuite.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+extern "C" char* analyzeTriangle(int side1, int side2, int side3);
+
+namespace Group7CollectiveTestSuite
+{
+	TEST_CLASS(Group7CollectiveTestSuite)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
